9cd43d92a1cbcf37667af99e0594c57fda6a2dd9
Ovanes <o-zakharyan@mail.ru>
2022-06-19 11:19:36 +0300

    create mainView for VKapp to SwiftUI

---
+++ TestSwiftUI/TestSwiftUI/TestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftUIApp.swift
+//  TestSwiftUI
+//
+//  Created by Ованес Захарян on 16.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
